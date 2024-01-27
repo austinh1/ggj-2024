2c5ef7acf78bc70d728fc9a0f07a5c840718d400
Austin <codexscript1@gmail.com>
2024-01-27 12:50:31 -0500

    Added car (though doesnt work for some reason)

---
+++ Assets/Car.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Rigidbody body;
+
+    private bool truckin = false;
+    private Vector3 forward;
+
+    private void Start()
+    {
+        forward = transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        if (truckin)
+        {
+            body.AddForce(forward * 100f);
+
+            if (transform.position.magnitude > 1000)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            truckin = true;
+        }
+    }
+}
