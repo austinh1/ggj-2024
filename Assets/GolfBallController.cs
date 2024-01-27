f541542d89cd9733e6e490c45e2ffa6002f28aae
Austin Huebner <codexscript1@gmail.com>
2024-01-26 22:11:26 -0500

    You can be the golf ball

--- Assets/GolfBallController.cs
+++ Assets/GolfBallController.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class GolfBallController : MonoBehaviour
 {
-    public Rigidbody rigidbody;
-    // Start is called before the first frame update
+    public Rigidbody body;
+    public float upwardVelocity = .25f;
+    public float forwardVelocity = 8f;
+    public float leftRotateSpeed = -1f;
+    public float rightRotateSpeed = 1f;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        var grounded = Physics.Raycast(transform.position, Vector3.down, 5f);
+        var grounded = Physics.Raycast(transform.position, Vector3.down, 1f);
         if (grounded && Input.GetMouseButtonDown(0))
         {
-            rigidbody.AddForce((transform.forward + new Vector3(0, 1, 0)) * 13f, ForceMode.Impulse);
+            body.AddForce((transform.forward + new Vector3(0, upwardVelocity, 0)) * forwardVelocity, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles += new Vector3(0, leftRotateSpeed, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, rightRotateSpeed, 0);
         }
     }
 }
