b20e1934f87fbeed9df4168c6a89bd95cb5504ee
Michael Kirkpatrick <tehcupcakes@hotmail.com>
2024-01-27 16:30:38 -0500

    Fix pins to stay stuck on player

--- Assets/Scripts/DespawnTimer.cs
+++ Assets/Scripts/DespawnTimer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DespawnTimer : MonoBehaviour
@@ -5,8 +6,27 @@ public class DespawnTimer : MonoBehaviour
     [Range(0f, 600f)]
     public float timeSeconds = 10f;
 
+    private Coroutine destroyCorutine;
+
     void Start()
     {
-        Destroy(gameObject, timeSeconds);
+        destroyCorutine = StartCoroutine(DestroyAfterDelay(timeSeconds));
+    }
+
+    // Coroutine to destroy the object after a delay
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+    public void Cancel()
+    {
+        if (destroyCorutine != null)
+        {
+            StopCoroutine(destroyCorutine);
+            // When it's cancelled, there's no way to start it again, so just remove the script
+            Destroy(this);
+        }
     }
 }
--- Assets/Scripts/StickOnCollide.cs
+++ Assets/Scripts/StickOnCollide.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class StickOnCollide : MonoBehaviour
 {
     private Rigidbody rigid;
+    private Collider collide;
 
     void Start()
     {
         rigid = transform.GetComponent<Rigidbody>();
+        collide = transform.GetComponent<Collider>();
     }
 
     private void OnCollisionEnter(Collision other)
@@ -16,6 +18,17 @@ public class StickOnCollide : MonoBehaviour
             transform.SetParent(other.transform, true);
             rigid.isKinematic = true;
             rigid.useGravity = false;
+
+            if (other.gameObject.CompareTag("Player"))
+            {
+                collide.enabled = false;
+                // Tacks attached to the player last FOREVER :)
+                var despawner = GetComponent<DespawnTimer>();
+                if (despawner != null)
+                {
+                    despawner.Cancel();
+                }
+            }
         }
     }
 }
