a364ee49ac157835348af58300d4fde543f75e33
Austin Huebner <codexscript1@gmail.com>
2024-01-28 16:36:14 -0500

    Added hurt trees for vr obstacle course

--- Assets/HurtTree.cs
+++ Assets/HurtTree.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HurtTree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.rigidbody.AddForce((other.transform.position - transform.position).normalized * 12f, ForceMode.Impulse);
+        }
     }
 }
