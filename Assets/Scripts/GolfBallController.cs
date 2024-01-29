0cebdf7e46eed831e0acd1e9558a4f8f25c29902
Austin <codexscript1@gmail.com>
2024-01-29 13:05:26 -0500

    Added restart button with R

--- Assets/Scripts/GolfBallController.cs
+++ Assets/Scripts/GolfBallController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GolfBallController : MonoBehaviour
 {
@@ -68,6 +69,11 @@ public class GolfBallController : MonoBehaviour
     {
         if (!meshRenderer.enabled)
             return;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
         
         var grounded = Physics.Raycast(transform.position, Vector3.down, groundRaycastDistance);
         if (grounded)
